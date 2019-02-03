3781ec8cb66aefd1a511d519a4c663f29aac11c0
Toseef <mtouseef.zafar@gmail.com>
2019-02-03 22:06:36 +0000

    added swagger ui config

--- src/main/java/com/birlasoft/priceservice/config/Config.java
+++ src/main/java/com/birlasoft/priceservice/config/Config.java
@@ -1,10 +1,18 @@
 package com.birlasoft.priceservice.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 import java.util.HashMap;
 import java.util.Map;
 
 @Configuration
+
 public class Config {
 
     public static Map<String, String> colorMapper = new HashMap<>();
